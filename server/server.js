e7304b9f38c2cd2e76f2d8d2cbfe0793babf815d
Lars Thorup <lars@zealake.com>
2017-06-03 15:31:28 +0200

    app: DreamList component

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const pgp = require('pg-promise')();
 // console.log('process.env.DATABASE', process.env.DATABASE);
 const db = pgp(process.env.DATABASE);
+const port = 3001; // Note: must match port of the "proxy" URL in app/package.json
 
 const app = express();
 
@@ -16,23 +17,23 @@ async function onGetDreams (request, response) {
   const dreamList = rowList.map(row => row.title);
   response.send(dreamList);
 }
-app.get("/dreams", onGetDreams);
+app.get("/api/dreams", onGetDreams);
 
 async function onDreamPosted (request, response) {
   const dream = request.query.dream;
   await db.query(`insert into dream (title) values ('${dream}')`);
   response.sendStatus(200);
 }
-app.post("/dreams", onDreamPosted);
+app.post("/api/dreams", onDreamPosted);
 
 async function onDeleteDream (request, response) {
   const dream = request.query.dream;
   await db.query(`delete from dream where title = '${dream}'`);
   response.sendStatus(200);
 }
-app.delete("/dreams", onDeleteDream);
+app.delete("/api/dreams", onDeleteDream);
 
 function listener () {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${port}`);
 }
-app.listen(process.env.PORT, listener);
+app.listen(port, listener);
