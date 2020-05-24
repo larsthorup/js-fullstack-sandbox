ddd62b36c002ef2c648eb4ae7ad2b298a3f3c238
Lars Thorup <lars@zealake.com>
2020-05-24 13:26:26 +0200

    React: useState & useEffect

--- app/src/DreamList.js
+++ app/src/DreamList.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from "react";
 
-class DreamList extends Component {
-  state = {
-    dreamList: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/.netlify/functions/dreams');
-    const dreamList = await response.json();
-    this.setState({dreamList});
-  }
-
-  render() {
-    return (
-      <div className="DreamList">
-        <h3>All my dreams</h3>
-        <ul>
-        {this.state.dreamList.map(dream =>
+const DreamList = () => {
+  const [dreamList, setDreamList] = useState([]);
+  useEffect(() => {
+    const fetchDreams = async () => {
+      const response = await fetch("/.netlify/functions/dreams");
+      const dreamList = await response.json();
+      setDreamList(dreamList);
+    };
+    fetchDreams();
+  }, []);
+  return (
+    <div>
+      <h3>All my dreams</h3>
+      <ul>
+        {dreamList.map((dream) => (
           <li key={dream.id}>{dream.title}</li>
-        )}
-        </ul>
-      </div>
-    );
-  }
-}
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default DreamList;
